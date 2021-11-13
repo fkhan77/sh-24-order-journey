104e55d0c329f96028cc87ae9458a63589a27c8e
Farhad Khan <farhadk1996@gmail.com>
2021-11-13 12:16:28 +0000

    Simplify Exports

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import { Heading } from "./Heading/Heading";
+import { MultiOptionQuestion } from "./MultiOptionQuestion/MultiOptionQuestion";
+import { NavigationBar } from "./NavigationBar/NavigationBar";
+import { NavigationButton } from "./NavigationButton/NavigationButton";
+import { Results } from "./Results/Results";
+import { SelectionChip } from "./SelectionChip/SelectionChip";
+import { TextInputQuestion } from "./TextInputQuestion/TextInputQuestion";
+
+export {
+  Heading,
+  MultiOptionQuestion,
+  NavigationBar,
+  NavigationButton,
+  Results,
+  SelectionChip,
+  TextInputQuestion,
+};
