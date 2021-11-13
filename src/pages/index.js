7a4879aab5742592a14bd48748b4cea208dec118
Farhad Khan <farhadk1996@gmail.com>
2021-11-13 12:20:12 +0000

    Add navigation logic. Add Formik. Add Yup.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,76 @@
-import Head from "next/head";
 import styled from "styled-components";
-import { TextInputQuestion } from "../components/TextInputQuestion/TextInputQuestion";
-import { NavigationBar } from "../components/NavigationBar/NavigationBar";
-import { MultiOptionQuestion } from "../components/MultiOptionQuestion/MultiOptionQuestion";
-import { Results } from "../components/Results/Results";
+import { useState } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import Head from "next/head";
+import {
+  TextInputQuestion,
+  NavigationBar,
+  MultiOptionQuestion,
+  Results,
+} from "../components";
 
 const HomeContainer = styled.div`
+  height: 12rem;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   margin-top: 2rem;
   padding-top: 1rem;
   padding-bottom: 1rem;
   width: 70%;
   margin: auto;
-  background-color: #2f2f81;
+  background-color: ${(props) => props.theme.primary};
   /* Large devices (Laptops, 1440px and down) */
   @media only screen and (max-width: 1440px) {
-    width: 80%;
-    background-color: purple;
+    width: 70%;
   }
   /* Medium devices (landscape tablets, 768px and down) */
   @media only screen and (max-width: 768px) {
     width: 90%;
-    background-color: green;
   }
 
   /* Extra small devices (phones, 600px and down) */
   @media only screen and (max-width: 600px) {
     width: 100%;
-    background-color: red;
   }
 `;
 
 export default function Home() {
+  const [step, setStep] = useState(0);
+  const sections = [
+    <TextInputQuestion
+      title="What is your name?"
+      label="Name"
+      key="NameQuestion"
+    />,
+    <TextInputQuestion
+      title="What is your email address?"
+      label="Email address"
+      key="EmailQuestion"
+    />,
+    <MultiOptionQuestion
+      title="What service are you here for?"
+      options={["STI Testing", "Contraception", "Other"]}
+      key="ServicesQuestion"
+    />,
+    <Results
+      results={[
+        { question: "Sample Question", answer: "Sample Answer" },
+        { question: "Sample Question 2", answer: "Sample Answer 2" },
+      ]}
+      key="Results"
+    />,
+  ];
+
+  const handleBack = () => {
+    setStep(step - 1);
+  };
+  const handleNext = () => {
+    setStep(step + 1);
+  };
+
   return (
     <div>
       <Head>
@@ -41,21 +78,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomeContainer>
-        <TextInputQuestion title="What is your name?" label="Name" />
-        <TextInputQuestion
-          title="What is your email address?"
-          label="Email address"
-        />
-        <MultiOptionQuestion
-          title="What service are you here for?"
-          options={["STI Testing", "Contraception", "Other"]}
-        />
-        <NavigationBar />
-        <Results
-          results={[
-            { question: "Sample Question", answer: "Sample Answer" },
-            { question: "Sample Question 2", answer: "Sample Answer 2" },
-          ]}
+        {sections[step]}
+        <NavigationBar
+          handleBack={handleBack}
+          handleNext={handleNext}
+          isFirstStep={step === 0}
+          isLastStep={step === sections.length - 1}
         />
       </HomeContainer>
     </div>
